2620135c58363a0b86bbc4c7146dd222e1ad791b
Storm Lin <stormsomething@gmail.com>
2023-03-04 20:24:43 -0500

    Starting Twilio Features

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,14 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSid = process.env.TWILIO_SERVICE_SID;
+const client = require('twilio')(accountSid, authToken);
+const express = require('express');
+const { MessagingResponse } = require('twilio').twiml;
 
 // ---------- SETUP ----------
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
@@ -35,3 +43,25 @@ app.get('/game', (req, res) => {
 
 // ---------- START APP ----------
 app.listen(process.env.PORT || 3000);
+
+function sendSMS(phoneNum, text) {
+    client.messages.create({
+        body: text,
+        messagingServiceSid: serviceSid,
+        to: phoneNum
+    }).then(message => console.log(message.sid));
+}
+
+function awaitSMS() {
+    app.post('/sms', (req, res) => {
+        const twiml = new MessagingResponse();
+      
+        twiml.message('Received!');
+      
+        res.type('text/xml').send(twiml.toString());
+    });
+      
+    app.listen(3000, () => {
+        console.log('Express server listening on port 3000');
+    });
+}
